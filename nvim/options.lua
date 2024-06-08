04dc42f996eefa2d6a18029a6b9f30935cbaff44
Austin D <shuffletick@gmail.com>
2024-06-07 19:01:29 -0600

    wsl; clipboard

--- nvim/options.lua
+++ nvim/options.lua
@@ -5,3 +5,16 @@
 -- LSP Server to use for Python.
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff_lsp"
+
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
