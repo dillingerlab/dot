fd9d415aaf9ec1ba34c139bb0bb4e5aa5ac4d1ef
Austin <shuffletick@gmail.com>
2024-09-26 18:53:21 -0600

    use ruff; correctly..

--- nvim/options.lua
+++ nvim/options.lua
@@ -2,9 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- LSP Server to use for Python.
--- vim.g.lazyvim_python_lsp = "pyright"
-vim.g.lazyvim_python_ruff = "ruff_lsp"
+vim.g.lazyvim_python_lsp = "ruff_lsp"
 
 vim.opt.swapfile = false
 vim.opt.backup = false
