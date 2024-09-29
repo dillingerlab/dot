79ffeeb83adcb33a5d7bd80df9535bb43446dbb1
Austin <shuffletick@gmail.com>
2024-09-28 20:24:51 -0600

    yank to system clipboard in normal mode

--- nvim/keymaps.lua
+++ nvim/keymaps.lua
@@ -5,6 +5,10 @@
 -- greatest remap ever - Prime
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- next greatest remap ever : asbjornHaland : prime
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
